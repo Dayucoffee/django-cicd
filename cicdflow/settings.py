ed860016a3198af734f264756b5c74671a1537ce
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2023-10-30 17:36:39 +0100

    Update settings.py

--- cicdflow/settings.py
+++ cicdflow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1b-b0grku3mf)cs-^ygcxxz)eb)ue-pwbecm3$*jr+^rzw$-^#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-github-actions-aws-env-1.eba-bwrkppgp.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['demo-env.eba-yacykpbq.us-east-2.elasticbeanstalk.com']
 
 
 # Application definition
